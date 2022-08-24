c6f1d17015d2cf51cb42e1a6cd235c1e7329d6a4
Salini <shalini021037@gmail.com>
2022-08-24 19:36:07 +0530

    file added

---
+++ 23AugExercises/Problem6.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23August
+{
+    internal class Problem6
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Enter deptname");
+            string dept_name = Console.ReadLine();
+            int k;
+
+            switch (dept_name)
+            {
+                case "Purchase":
+                    k = (int)deptnames.Purchase;
+                    Console.WriteLine("Dept NO: " +k);
+                    break;
+                case "Sales":
+                    k = (int)deptnames.Sales;
+                    Console.WriteLine("Dept NO: " + k);
+                    break;
+                case "Training":
+                    k = (int)deptnames.Training;
+                    Console.WriteLine("Dept NO: " + k);
+                    break;
+                case "Accounts":
+                    k = (int)deptnames.Accounts;
+                    Console.WriteLine("Dept NO: " + k);
+                    break;
+                default:
+                    break;
+
+            }
+            Console.ReadLine();
+            
+        }
+    
+        enum deptnames 
+        {
+            Purchase = 1, 
+            Sales = 2, 
+            Training = 3,
+            Accounts = 4
+        }
+    }
+}
