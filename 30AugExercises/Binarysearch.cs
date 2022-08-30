ca0b8850f60962711c51356d356f2647393055e6
Salini <shalini021037@gmail.com>
2022-08-30 16:22:33 +0530

    files added

---
+++ 30AugExercises/Binarysearch.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30August
+{
+    internal class Binarysearch
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];      
+            
+            AcceptSortArray(arr);
+            //PrintSortedArray(arr);
+            Console.WriteLine("Find element to search");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            BinarySearch(arr, 1, arr.Length, f);
+            Console.Read();
+
+        }
+
+        internal static void BinarySearch(int[] a, int i, int j, int k)
+        {
+
+            int mid;
+            while (i <= j)
+            {
+                mid = (i + j) / 2;
+                if (a[mid] == k)
+                {
+                    Console.WriteLine("Value found at the middle =  " + mid);
+                    break;
+                }
+                else if (a[mid] > k)
+                {
+                    j = mid - 1;
+                    Console.WriteLine("Value found at =  " + j);
+                    break;
+                }
+                else
+                    i = mid + 1;
+                Console.WriteLine("Value found at  =  " + i);
+                break;
+            }
+        }
+
+        internal static void AcceptSortArray(int[] arr)
+        {
+            //int temp, j;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter element");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int  j, temp;
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                temp = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] >= temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+            Console.WriteLine("Sorted Array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.WriteLine();
+
+        }
+
+        //internal static void PrintSortedArray(int[] arr) 
+        //{ 
+        //    Console.WriteLine("Sorted Array");
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        Console.Write(arr[i]+"\t");
+        //    }
+        //    Console.WriteLine();
+        //}
+    }
+}
---
+++ 30AugExercises/Insertion sort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30August
+{
+    internal class Insertion_sort
+    {
+        static void Insert(int[] arr)
+        {
+
+            int i, j, temp;
+            int n = arr.Length;
+            for (i = 1; i < n; i++)
+            {
+                temp = arr[i];
+                j = i - 1;
+                while (j >= 0 &&  arr[j]>= temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+
+        static void PrintSortedArray(int[] arr)
+        {
+
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]+"\t");
+            }
+            Console.WriteLine();
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] ;
+            int n = arr.Length;
+            
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the array element");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Before Sorting");
+            PrintSortedArray(arr);
+            Insert(arr);
+            Console.WriteLine("After Sorting");
+            Console.ForegroundColor = ConsoleColor.Green;
+            PrintSortedArray(arr);
+            Console.Read(); 
+        }
+    }
+}
