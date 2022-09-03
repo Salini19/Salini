cc47c04bfd02b4bee3013064e87aeb1dd16647a9
Salini <shalini021037@gmail.com>
2022-09-03 22:31:15 +0530

    files added

---
+++ Sep2/Category.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class Category
+    {
+		private int _catid;
+
+		public int CategoryID
+		{
+			get { return _catid; }
+			set { _catid = value; }
+		}
+		private string _catname;
+
+		public string CategoryName
+		{
+			get { return _catname; }
+			set { _catname = value; }
+		}
+
+		private string _description;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+       
+		private List<Product> _productlist;
+
+		public List<Product> Products
+		{
+			get { return _productlist; }
+			set { _productlist = value; }
+		}
+
+	}
+}
---
+++ Sep2/Product.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class Product
+    {
+		private int _pid;
+
+		public int ProductID
+		{
+			get { return _pid; }
+			set { _pid = value; }
+		}
+
+		private string _pname;
+
+		public string ProductName
+		{
+			get { return _pname; }
+			set { _pname = value; }
+		}
+
+		private int _qtysold;
+
+		public int QtySold
+		{
+			get { return _qtysold; }
+			set { _qtysold = value; }
+		}
+
+		private int _unitprice;
+
+		public int UnitPrice
+		{
+			get { return _unitprice; }
+			set { _unitprice = value; }
+		}
+
+		private int _unitofmeasurement;
+
+		public int UnitOFMeasurement
+		{
+			get { return _unitofmeasurement; }
+			set { _unitofmeasurement = value; }
+		}
+
+		private int _qtyinhand;
+
+		public int QtyInHand
+		{ 
+			get { return _qtyinhand; }
+			set { _qtyinhand = value; }
+		}
+
+		private int _reoder;
+
+		public int ReorderLevel
+		{
+			get { return _reoder; }
+			set { _reoder = value; }
+		}
+
+
+
+	}
+}
