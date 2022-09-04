cf0aa06b67a75f72be787af6772686d00c81dbb4
Salini <shalini021037@gmail.com>
2022-09-04 23:34:30 +0530

    files modified

--- Sep2/Product.cs
+++ Sep2/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HRLibrary
@@ -40,9 +41,9 @@ namespace HRLibrary
 			set { _unitprice = value; }
 		}
 
-		private int _unitofmeasurement;
+		private string _unitofmeasurement;
 
-		public int UnitOFMeasurement
+		public string UnitOFMeasurement
 		{
 			get { return _unitofmeasurement; }
 			set { _unitofmeasurement = value; }
@@ -63,8 +64,53 @@ namespace HRLibrary
 			get { return _reoder; }
 			set { _reoder = value; }
 		}
+		public Product()
+		{
 
+		}
 
+		public Product(int pid,string pname,int qtysold,int unitp,string meas,int qtyinhand,int reorder)
+		{
+			_pname = pname;
+			_pid = pid;
+			_qtysold = qtysold;
+			_unitprice=unitp;
+			_unitofmeasurement=meas;
+			_qtyinhand = qtyinhand;
+			_reoder=reorder;
+		}
+		
+
+		public void AddProduct(Product product ,Category category1) //,int prodid,string prodname ,int qtsold ,int unitp,int unitmeasure,int qtinhand,int reorderlvl)
+		{
+			
+			Console.WriteLine("Enter product ID");
+			int prodid = int.Parse(Console.ReadLine());
+			product.ProductID = prodid;
+			Console.WriteLine("enter product name");
+			string prodname = Console.ReadLine();
+			product.ProductName = prodname;
+            Console.WriteLine("Enter amount of QunatitySold");
+            int qtsold = int.Parse(Console.ReadLine());
+            product.QtySold = qtsold;
+            Console.WriteLine("Enter the amount of unitprice");
+            int unitp = int.Parse(Console.ReadLine());
+            product.UnitPrice = unitp;
+            Console.WriteLine("Enter the unit of measurement");
+			string unitmeasure = Console.ReadLine();
+            product.UnitOFMeasurement = unitmeasure;
+            Console.WriteLine("Enter the amount of quantity in hand");
+            int qtinhand = int.Parse(Console.ReadLine());
+            product.QtyInHand = qtinhand;
+            Console.WriteLine("Enter Reorderlevel");
+            int reorderlvl = int.Parse(Console.ReadLine());
+            product.ReorderLevel = reorderlvl;
+			Console.WriteLine("-----------------------------------------");
+			
+			
+		}
+
+		
 
 	}
 }
