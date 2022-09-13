91f29a07e6641a523afb83c77608c1b4d3748607
Salini <shalini021037@gmail.com>
2022-09-14 01:59:35 +0530

    Files added

---
+++ Sep13/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserModule
+{
+    public class Movie
+    {
+		private string _lang;
+
+		public string Language
+		{
+			get { return _lang; }
+			set { _lang = value; }
+		}
+		private string _genre;
+
+		public string Genre
+		{
+			get { return _genre; }
+			set { _genre = value; }
+		}
+		private string _name;
+
+		public string MovieName
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		
+	}
+}
---
+++ Sep13/User.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserModule
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        private int _phno;
+
+        public int PhoneNumber
+        {
+            get { return _phno; }
+            set { _phno = value; }
+        }
+        public DateTime AddedDate { get; set; }
+
+        private List<Movie> _moviestock;
+
+        public List<Movie> MovieStock
+        {
+            get { return _moviestock; }
+            set
+            {
+                if (MovieStock.Count >= 5) { _moviestock = value; }
+                else
+                {
+                    Console.WriteLine("No more rental is allowed if the limit is reached");
+                }
+            }
+        }
+        public void Display()
+        {
+            if (MovieStock.Count == 2)
+                Console.WriteLine("Hello User you have a Silver Badge");
+            if (MovieStock.Count == 3)
+                Console.WriteLine("Hello User you have a Gold Badge");
+            if (MovieStock.Count == 5)
+                Console.WriteLine("Hello User you have a Platinum Badge");
+
+
+        }
+    }
+    enum Seats
+    {
+        Silver = 2,
+        Gold=3,
+        Platinum=5
+    }
+
+
+    public class SilverUser : User
+    {
+        private List<Movie> _moviestock;
+
+        public new List<Movie> MovieStock
+        {
+            get { return _moviestock; }
+            set
+            {
+                if (MovieStock.Count == 2) { _moviestock = value; }
+
+            }
+        }
+
+
+    }
+
+    public class GoldUser : SilverUser
+    {
+        private List<Movie> _moviestock;
+
+        public new List<Movie> MovieStock
+        {
+            get { return _moviestock; }
+            set
+            {
+                if (MovieStock.Count == 3) { _moviestock = value; }
+
+            }
+        }
+
+    }
+
+    public class PlatinumUser : GoldUser
+    {
+        private List<Movie> _moviestock;
+
+        public new List<Movie> MovieStock
+        {
+            get { return _moviestock; }
+            set
+            {
+                if (MovieStock.Count == 5) { _moviestock = value; }
+
+            }
+        }
+
+
+    }
+}
